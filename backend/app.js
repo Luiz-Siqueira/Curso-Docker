8d881a4c912318064f7b3b7945aaf64202dc2e8e
Luiz-Siqueira <luiz.gomees400@gmail.com>
2022-01-27 20:32:39 -0300

    Finalizando backend  | Finalizando frontend

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,32 @@ const restful = require('node-restful')
 const server = express()
 const mongoose = restful.mongoose
 
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
 //DataBase
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://db/mydb',{useMongoClient:true});
 
-server.get('/', (req,res,next) => res.send('Backend'))
+server.use(bodyParser.urlencoded({extended:true}))
+server.use(bodyParser.json())
+server.use(cors())
+
+//ODM
+
+const client = restful.model('Client',{
+    name:{type: String, required:true}
+})
+
+
+//api rest
+client.methods(['get','post','put','delete'])
+client.updateOptions({new:true,runValidators:true})
+
+//Routes
+client.register(server,'/client')
+
 
 //start server
-
 server.listen(3000)
 
